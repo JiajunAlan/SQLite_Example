b3b6266e7df9a0aa523ab83e8776478fd0ee03a4
JiajunAlan <alan233@my.yorku.ca>
2021-07-17 19:38:46 -0400

    Update CustomerModel.java

--- main/java/com/jiajun/sqlite_example/CustomerModel.java
+++ main/java/com/jiajun/sqlite_example/CustomerModel.java
@@ -1,8 +1,60 @@
 package com.jiajun.sqlite_example;
 
 public class CustomerModel {
+    private int id;
     private String name;
     private int age;
     private boolean isPremium;
-    
+
+    public CustomerModel(int id, String name, int age, boolean isPremium) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.isPremium = isPremium;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", isPremium=" + isPremium +
+                '}';
+    }
+
+    public CustomerModel() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isPremium() {
+        return isPremium;
+    }
+
+    public void setPremium(boolean premium) {
+        isPremium = premium;
+    }
 }
