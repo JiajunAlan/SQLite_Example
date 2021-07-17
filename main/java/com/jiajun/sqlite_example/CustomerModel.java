37926c49528e9d884fb247a00221dcf091b535c1
JiajunAlan <alan233@my.yorku.ca>
2021-07-17 19:36:57 -0400

    CustomerModel added

---
+++ main/java/com/jiajun/sqlite_example/CustomerModel.java
@@ -0,0 +1,8 @@
+package com.jiajun.sqlite_example;
+
+public class CustomerModel {
+    private String name;
+    private int age;
+    private boolean isPremium;
+    
+}
--- main/java/com/jiajun/sqlite_example/MainActivity.java
+++ main/java/com/jiajun/sqlite_example/MainActivity.java
@@ -1,14 +1,28 @@
 package com.jiajun.sqlite_example;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+        Button btn_Add, btn_ViewAll;
+        EditText et_Name, et_Age;
+        RecyclerView rv_Result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_Add = findViewById(R.id.btn_Add);
+        btn_ViewAll = findViewById(R.id.btn_ViewAll);
+
+        et_Age = findViewById(R.id.input_Age);
+        et_Name = findViewById(R.id.input_Name);
+
+        rv_Result = findViewById(R.id.rv_result);
     }
+
 }
